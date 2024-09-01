901789460323cb1b95c5cd9a864d073004b382ad
fxfx <vinhhp@vhttech.com>
2024-09-01 15:56:00 +0700

    update

--- scriptinit.sh
+++ scriptinit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
-#tar -xvf node-v20.17.0-linux-x64.tar.xz 
-#mv node-v20.17.0-linux-x64 ~/node/
-#rm -rf node-v20.17.0-linux-x64.tar.xz
+wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
+tar -xvf node-v20.17.0-linux-x64.tar.xz 
+mv node-v20.17.0-linux-x64 ~/node/
+rm -rf node-v20.17.0-linux-x64.tar.xz
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
--- synctodesk.sh
+++ synctodesk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -arvz --progress .config/nvim/lua/configs/ ~/.config/nvim/lua/configs
-rsync -arvz --progress .local/share/gnome-shell/extensions/ ~/.local/share/gnome-shell/extensions
+rsync -arvz --progress .config ~/
+rsync -arvz --progress .local ~/
 
