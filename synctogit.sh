c6308760ab716b6ced8cd85a5c33b4ac2a509847
FxFx <vinhhp@vhttech.com>
2024-09-01 15:40:29 +0700

    update

--- synctogit.sh
+++ synctogit.sh
@@ -8,5 +8,7 @@ rsync -arvz --progress --delete ~/.local/share/icons .local/share/
 rsync -arvz --progress --delete ~/.local/share/themes .local/share/ 
 
 mkdir -p .config/nvim/lua/configs
+mkdir -p .config/tmux/
 rsync -arvz --progress --delete ~/.config/nvim/lua/configs/lspconfig.lua .config/nvim/lua/configs
+rsync -arvz --progress --delete ~/.config/tmux/tmux.conf .config/tmux/
 
