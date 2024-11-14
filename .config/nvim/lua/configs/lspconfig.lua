18ff9154fe49bbbbc4b3ff8a58d5af1173be0963
fxfx <vinhhp@vhttech.com>
2024-11-14 13:28:17 +0700

    update

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls", "rust_analyzer","bashls","tsserver" }
+local servers = { "html", "cssls", "gopls", "rust_analyzer","bashls","tsserver","jedi_language_server" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
