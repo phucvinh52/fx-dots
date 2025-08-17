a72836733b26afc6db6ceb0ca629b5e59ff2c531
FX <kiemsi.lv@gmail.com>
2025-08-17 23:26:34 +0700

    u

--- scriptinit.sh
+++ scriptinit.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-wget https://nodejs.org/dist/v20.17.0/node-v20.17.0-linux-x64.tar.xz
-tar -xvf node-v20.17.0-linux-x64.tar.xz 
-mv node-v20.17.0-linux-x64 ~/node/
-rm -rf node-v20.17.0-linux-x64.tar.xz
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
+VERSION_NODE="v22.18.0"
+wget https://nodejs.org/dist/${VERSION_NODE}/node-${VERSION_NODE}-linux-x64.tar.xz
+tar -xvf node-${VERSION_NODE}-linux-x64.tar.xz 
+sudo rm -rf ~/node
+mv node-${VERSION_NODE}-linux-x64 ~/node/
+rm -rf node-${VERSION_NODE}-linux-x64.tar.xz
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage --appimage-extract
+sudo rm -rf /squashfs-root
 sudo mv squashfs-root /
+sudo rm -rf /usr/bin/nvim
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-rm -rf nvim.appimage
+rm -rf nvim-linux-x86_64.appimage
 
